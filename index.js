7234b97544cb5f13bb69e666afed8f88306efe86
Young Min Kim <youngmink@netflix.com>
2019-08-09 13:28:39 -0700

    Add working task

---
+++ index.js
@@ -0,0 +1,55 @@
+const { yaml, json, install } = require("mrm-core");
+
+const packages = [
+  "@typescript-eslint/eslint-plugin",
+  "@typescript-eslint/parser"
+];
+
+function task(config) {
+  config.defaults({ eslintConfig: ".eslintrc.js" });
+
+  const { eslintConfig: eslintConfigFileName } = config.values();
+
+  const eslintConfig = findESLintConfig(eslintConfigFileName);
+
+  const toBeMerged = {
+    parser: "@typescript-eslint/parser",
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  };
+
+  toBeMerged["plugins"] = eslintConfig.get("plugins", []);
+  toBeMerged["plugins"].push("@typescript-eslint");
+
+  toBeMerged["extends"] = eslintConfig.get("extends", []);
+  toBeMerged["extends"].unshift(
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript"
+  );
+
+  eslintConfig.merge(toBeMerged).save();
+
+  // Dependencies
+  install(packages);
+}
+task.description = "Adds TypeScript ESLint";
+
+function findESLintConfig(eslintConfigFileName) {
+  if (eslintConfigFileName) {
+    return eslintConfigFileName.endsWith(".yaml") ||
+      eslintConfigFileName.endsWith(".yml")
+      ? yaml(eslintConfigFileName)
+      : json(eslintConfigFileName);
+  }
+
+  if (yaml(".eslintrc.yaml")) return yaml(".eslintrc.yaml");
+  if (yaml(".eslintrc.yml")) return yaml(".eslintrc.yml");
+  if (json(".eslintrc.json")) return json(".eslintrc.json");
+  if (json(".eslintrc")) return json(".eslintrc");
+  return yaml(".eslintrc.yaml");
+}
+
+module.exports = task;
---
+++ index.test.js
@@ -0,0 +1,79 @@
+jest.mock("fs", () => require("memfs"));
+jest.mock("mrm-core/src/util/log", () => ({
+  added: jest.fn()
+}));
+const mockInstall = jest.fn();
+jest.mock("mrm-core/src/npm", () => ({
+  install: mockInstall
+}));
+
+const { getConfigGetter } = require("mrm");
+const vol = require("memfs").vol;
+const task = require("./index");
+
+const stringify = o => JSON.stringify(o, null, "  ");
+
+afterEach(() => vol.reset());
+
+const scaffold = ({ caseName, fsStatus = {}, eslintConfig }) => {
+  describe(`for an ${caseName}`, () => {
+    beforeAll(() => {
+      vol.fromJSON(fsStatus);
+
+      task(getConfigGetter({ eslintConfig }));
+    });
+
+    it(`should add TypeScript ESLint to ${caseName}`, () => {
+      expect(vol.toJSON()).toMatchSnapshot();
+    });
+
+    it("should install TypeScript ESLint plugins", () => {
+      expect(mockInstall).toHaveBeenCalledWith([
+        "@typescript-eslint/eslint-plugin",
+        "@typescript-eslint/parser"
+      ]);
+    });
+  });
+};
+
+[
+  {
+    caseName: "empty .eslintrc.yaml"
+  },
+  {
+    caseName: "preexisting .eslintrc.yaml",
+    fsStatus: {
+      "./.eslintrc.yaml": stringify("")
+    },
+    eslintConfig: "./.eslintrc.yaml"
+  },
+  {
+    caseName: "preexisting .eslintrc.json",
+    fsStatus: {
+      "./.eslintrc.json": stringify("")
+    },
+    eslintConfig: "./.eslintrc.json"
+  },
+  {
+    caseName: "preexisting .eslintrc",
+    fsStatus: { "./eslintrc": stringify({}) },
+    eslintConfig: "./.eslintrc"
+  },
+  {
+    caseName: "preexisting .eslintrc 2",
+    fsStatus: {
+      "./.eslintrc": stringify({
+        extends: ["airbnb", "prettier"],
+        parser: "babel-eslint",
+        plugins: ["react", "prettier"],
+        parserOptions: {
+          ecmaFeatures: {
+            experimentalObjectRestSpread: 1
+          },
+          sourceType: "script"
+        }
+      })
+    },
+    eslintConfig: "./.eslintrc"
+  }
+].forEach(scaffold);
